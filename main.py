8efbbfa8a86e1a790b43362bf9e8e054971c3b93
Tanguy9862 <tanguy98.surowiec@gmail.com>
2024-04-21 19:43:11 +0200

    Refactor scraping logic and enhance data processing
    - Refactor main scraping loop to improve efficiency and readability.
    - Integrate tqdm for progress tracking during the scraping process.
    - Extract functionality into `utils.py` for better modularity:
      - `extract_generic_and_admin` to parse drug information more accurately.
      - `clean_company_name` to normalize company names upon scraping.
    - Update `clean_mode_administration` function to handle edge cases in mode descriptions.
    - Implement logging to provide real-time status updates and completion messages.
    - Adjust CSV handling to dynamically append new data without header repetition.

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
 import logging
 import os
 
-import requests
-import pandas as pd
+from bs4 import BeautifulSoup
 from datetime import datetime
-from time import sleep
-from bs4 import BeautifulSoup, NavigableString
+import pandas as pd
+import requests
+from tqdm import tqdm
 
-# Regarder si j'ai déjà des données existantes
+from utils import extract_generic_and_admin, clean_company_name
+
+# Initialize constants: current and end years for scraping, date format, and browser headers
+CURRENT_YEAR, END_YEAR = int(datetime.utcnow().year), 2002
+DATE_FORMAT = '%B %d, %Y'
+headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"}
+logging.basicConfig(level=logging.INFO)
+
+# Check for existing CSV file to determine if we need to update or initialize data
 csv_file = 'new_drug_approvals.csv'
 file_exists = os.path.isfile(csv_file)
 
 if file_exists:
-    # RECUPERER LA DERNIERE ANNEE
-    pass
+    df = pd.read_csv(csv_file)
+    df['Date of Approval'] = pd.to_datetime(df['Date of Approval'], format='%Y-%m-%d', errors='coerce')
+    df_sorted = df.sort_values(by='Date of Approval', ascending=False)
+    most_recent = df_sorted.iloc[0]
+    most_recent_date, most_recent_drug = most_recent['Date of Approval'], most_recent['drug_name']
 else:
-    last_date = None
+    most_recent_date, most_recent_drug = None, None
 
-CURRENT_YEAR, END_YEAR = int(datetime.utcnow().year), 2002
-headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"}
-# Commencer à l'année actuelle
-
-
-# TODO :
-# Pour les noms génériques, ne chopper seulement que ce qui est entre parenthèses, pas le reste
-# En fait les parenthèses c'est le générique et la suite c'est le mode d'adm
-# Donc faire un split pour aussi récupérer le mode d'adm
-
-i = 0
-CURRENT_YEAR = 2023
+# Flag to determine when to stop scraping based on new data availability
+has_to_stop = False
 
 while CURRENT_YEAR >= END_YEAR:
 
-    # if i <= 15:
-    response = requests.get(f'https://www.drugs.com/newdrugs-archive/{CURRENT_YEAR}.html', headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # print(soup)
+    if not has_to_stop:
 
-    # Retrieve all drug blocks contained within the main 'ddc-media-list' div
-    all_drugs = soup.select_one('div.ddc-media-list').find_all('div', class_='ddc-media')
-    for drug in reversed(all_drugs):
-        new_data = dict()
-        print('************')
-        # print(drug)
+        logging.info(f'Scraping new drug approvals for {CURRENT_YEAR}')
+        response = requests.get(f'https://www.drugs.com/newdrugs-archive/{CURRENT_YEAR}.html', headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        drug_tag = drug.find('h3', class_='ddc-media-title')
-        new_data['drug_name'] = drug_tag.find('a').text if drug_tag.find('a') else drug_tag.text.split('(')[0]
-        new_data['drug_generic_name'] = ''.join([t for t in drug_tag if isinstance(t, NavigableString)]).strip()
+        # Retrieve all drug blocks contained within the main 'ddc-media-list' div
+        all_drugs = soup.select_one('div.ddc-media-list').find_all('div', class_='ddc-media')
+        for drug in tqdm(reversed(all_drugs)):
 
-        print(drug_tag.find('a').text if drug_tag.find('a') else drug_tag.text)
+            new_data = dict()
 
-        metadata_headers = ['Date of Approval:', 'Company:', 'Treatment for:']
-        for header in metadata_headers:
-            header_tag = drug.find('b', string=header)
-            if header_tag:
-                value = header_tag.next_sibling.strip()
-                print(value)
-                new_data[header.split(':')[0]] = value
+            # Get the content of the parent div from the current drug
+            drug_tag = drug.find('h3', class_='ddc-media-title')
 
-        drug_df = pd.DataFrame([new_data])
-        drug_df.to_csv(csv_file, mode='a', header=not file_exists, index=False)
-        file_exists = True
+            # Get the drug name
+            drug_name = drug_tag.find('a').text if drug_tag.find('a') else drug_tag.text.split('(')[0]
+            new_data['drug_name'] = drug_name
 
-        # print(new_data)
+            # Get the generic name and the mode of administration
+            generic_name, mode_administration = extract_generic_and_admin(str(drug_tag))
+            new_data['drug_generic_name'] = generic_name
+            new_data['mode_administration'] = mode_administration
 
-        # break
+            # Get the description
+            subtitle_tag = drug.find('p', class_='drug-subtitle')
+            description_tag = subtitle_tag.find_next_sibling('p', class_=False)
+            description = (description_tag or None) and description_tag.get_text(strip=True)
+            new_data["description"] = description
 
-    CURRENT_YEAR -= 1
-        # i += 1
+            # Iterate through headers to extract key data from <b> tags, which uniquely contain
+            # the metadata like approval date, company name, and treatment information
+            metadata_headers = ['Date of Approval:', 'Company:', 'Treatment for:']
+            for header in metadata_headers:
+                header_tag = drug.find('b', string=header)
+                if header_tag:
+                    value = header_tag.next_sibling.strip()
 
-    # break
-    # Boucle pour chaque médicament
-    # tmp_data
-    # Scraper info générale
-    # On scrape la date, si c'est la même que la dernière date du csv existant alors on break
-    # Récupérer url de la fiche du médicament
-    # Appel de l'agent LLM pour récupérer info secondaires
-    # Enregistrer les nouvelles données dans tmp_data
-    # Si tmp_data représente 10 % == 0 alors enregistre dans le csv (= toutes les 10 entrées)
-    # Clear tmp_data
+                    # Check if the current drug and its approval date match the most recently scraped drug and date:
+                    if header == 'Date of Approval:':
+                        date_formatted = datetime.strptime(value, DATE_FORMAT)
 
-# retour début de boucle
-# Une fois que j'ai terminé la boucle alors année - 1
+                        # If the current drug and its approval date match the most recently recorded drug and date,
+                        # it indicates there are no new updates to scrape, and the process should stop.
+                        if drug_name == most_recent_drug and date_formatted == most_recent_date:
+                            logging.info(f'{most_recent_drug} was the last drug scraped previously.\n'
+                                         f'Scraping has now been completed.')
+                            has_to_stop = True
+                            break
+
+                    # Otherwise, add scraped data to `new data`
+                    new_data[header.split(':')[0]] = value if header != 'Company:' else clean_company_name(value)
+
+            if has_to_stop:
+                break
+
+            # Append new scraped data to the existing CSV
+            drug_df = pd.DataFrame([new_data])
+            drug_df['Date of Approval'] = pd.to_datetime(drug_df['Date of Approval'], format=DATE_FORMAT)
+            drug_df.to_csv(csv_file, mode='a', header=not file_exists, index=False)
+            if not file_exists:
+                file_exists = True
+
+        CURRENT_YEAR -= 1
+
+    else:
+        break
---
+++ utils.py
@@ -0,0 +1,83 @@
+import re
+
+
+def clean_mode_administration(mode: str) -> str:
+    """
+    Cleans the mode of administration string by removing any parts
+    with 'formerly' and removing 'for' if it's the first word.
+
+    Args:
+        mode (str): The original mode of administration text.
+
+    Returns:
+        str: The cleaned mode of administration.
+    """
+
+    mode = re.sub(r'\s*-\s*formerly.*', '', mode)
+    mode = re.sub(r'^for\s+', '', mode, flags=re.IGNORECASE)
+    return mode.strip()
+
+
+def extract_generic_and_admin(drug_tag: str) -> tuple:
+    """
+    Extracts the generic name and mode of administration from a drug tag string.
+
+    Args:
+        drug_tag (str): The string containing the drug information typically in an HTML-like format.
+
+    Returns:
+        tuple: A tuple containing the generic name and cleaned mode of administration,
+               or (None, None) if no data is found.
+    """
+
+    # Try to capture nested parentheses and then text following until tag close or end of string
+    match = re.search(r'\(((?:[^\(\)]|\([^\)]*\))*)\)\s*(.*)', drug_tag)
+
+    if match:
+        generic_name = match.group(1).strip()
+        mode_administration = match.group(2).strip()
+
+        # Handle empty parentheses case
+        if not generic_name:
+            generic_name = None
+
+        mode_administration = re.sub(r'<\/?\w+>', '', mode_administration).strip()
+
+        return generic_name, clean_mode_administration(mode_administration)
+
+    return None, None
+
+
+def clean_company_name(company_name: str) -> str:
+    """
+    Cleans the company name by removing specified suffixes and special characters.
+
+    Args:
+        company_name (str): The original company name.
+
+    Returns:
+        str: The cleaned company name.
+    """
+
+    # List of suffixes to remove from the company name
+    suffixes_to_remove = [
+        r'\.', r'\,', r'\binc\b', r'\bltd\b', r'\bglobal\b', r'\blimited\b',
+        r'\bllc\b', r'\bcorp\b', r'\ba\/s\b', r'\bcorporation\b', r'\bs\.a\b',
+        r'\bgmbh\b', r'\bag\b', r'\bplc\b', r'\band co\b', r'\band company\b',
+        r'\bl\.p\b', r'& co', r'\bsa\b', r'\busa\b', r'\blp\b', r'\bus\b', r'\bincorporated\b',
+        r'\bpharma\b', r'\blaboratories\b'
+    ]
+
+    company_name = company_name.lower()
+
+    # Remove special characters such as periods and commas
+    company_name = re.sub(r'[.,]', '', company_name)
+
+    for suffix in suffixes_to_remove:
+        company_name = re.sub(suffix, "", company_name).strip()
+
+    return company_name
+
+
+
+

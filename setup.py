cc3ef710950c9d483809eed535acde015c8799aa
Tanguy9862 <tanguy98.surowiec@gmail.com>
2024-05-03 01:19:18 +0200

    Package scraper for deployment and reusability in Dash applications

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='new_drug_approvals_scraper',
+    version='0.1.0',
+    author='SUROWIEC Tanguy',
+    description='A comprehensive package for scraping and processing new drug approval data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Tanguy9862/scraper-new-drug-approvals',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'requests~=2.31.0',
+        'tqdm~=4.66.2',
+        'pandas~=2.2.2',
+        'langchain~=0.1.16',
+        'openai',
+        'python-dotenv~=1.0.1',
+        'langchain-openai'
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'scrape-drugs=new_drug_approvals.main:scrape_new_drug_approvals_data'
+        ],
+    },
+)
